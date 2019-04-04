ba84299a12723ff43bd9c33a32278acfa0f1c16e
Connor Prussin <connor@prussin.net>
2019-04-04 14:52:23 -0700

    Use purescript-0.12.3 in nix shell file

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 {
-  purescript-version ? "0.12.2",
-  nixjs-version ? "0.0.6",
+  purescript-version ? "0.12.3",
+  nixjs-version ? "0.0.8",
   nixjs ? fetchTarball "https://github.com/cprussin/nixjs/archive/${nixjs-version}.tar.gz",
   nixpkgs ? <nixpkgs>
 }:
